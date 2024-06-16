3c97fa5dbda54c2a04a819585d9bb11436fd2440
FEYZA OZKEFE <feyza.ozkefe@etstur.com>
2024-06-16 16:27:25 +0300

    guesttable dtos added

---
+++ SignalR.DtoLayer/GuestTableDto/ResultGuestTableDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.DtoLayer.GuestTableDto
+{
+    public class ResultGuestTableDto
+    {
+        public int GuestTableId { get; set; }
+        public string Name { get; set; }
+        public bool Status { get; set; }
+    }
+}
--- SignalRApi/Controllers/GuestTablesController.cs
+++ SignalRApi/Controllers/GuestTablesController.cs
@@ -20,5 +20,56 @@ namespace SignalRApi.Controllers
         {
             return Ok(_guestTableService.TGuestTableCount());
         }
+
+
+        [HttpGet]
+        public IActionResult GuestTableList()
+        {
+            var values = _guestTableService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateGuestTable(CreateAboutDto createAboutDto)
+        {
+            About about = new About()
+            {
+                Title = createAboutDto.Title,
+                Description = createAboutDto.Description,
+                ImageUrl = createAboutDto.ImageUrl,
+            };
+
+            _guestTableService.TAdd(about);
+            return Ok("Hakkında Kısmı Başarılı Bir Şekilde Eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGuestTable(int id)
+        {
+            var value = _guestTableService.TGetById(id);
+            _guestTableService.TDelete(value);
+            return Ok("Hakkında Alanı Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateGuestTable(UpdateAboutDto updateAboutDto)
+        {
+            About about = new About()
+            {
+                AboutId = updateAboutDto.AboutId,
+                Title = updateAboutDto.Title,
+                Description = updateAboutDto.Description,
+                ImageUrl = updateAboutDto.ImageUrl,
+            };
+            _guestTableService.TUpdate(about);
+            return Ok("Hakkında Alanı Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGuestTable(int id)
+        {
+            var value = _guestTableService.TGetById(id);
+            return Ok(value);
+        }
     }
 }
